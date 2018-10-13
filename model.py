73c6f8b30763ba982805f24c489af7dfe54a706c
Joan Karadimov <joan.karadimov@gmail.com>
2018-10-13 11:08:42 +0300

    Remove an optimization and simplify some code

--- model.py
+++ model.py
@@ -28,14 +28,13 @@ class Model(object):
         self.targets = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
         self.initial_state = self.cell.zero_state(args.batch_size, tf.float32)
 
-        with tf.variable_scope('rnnlm'):
-            softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
-            softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
-            with tf.device("/cpu:0"):
-                embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
-                inputs = tf.nn.embedding_lookup(embedding, self.input_data)
-                inputs = tf.split(inputs, args.seq_length, axis=1)
-                inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+        softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+        softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
+
+        embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
+        inputs = tf.split(inputs, args.seq_length, axis=1)
+        inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
         def loop(prev, _):
             prev = tf.nn.xw_plus_b(prev, softmax_w, softmax_b)
@@ -43,7 +42,7 @@ class Model(object):
             return tf.nn.embedding_lookup(embedding, prev_symbol)
 
         outputs, last_state = seq2seq.rnn_decoder(inputs, self.initial_state, self.cell,
-                                                  loop_function=loop if infer else None, scope='rnnlm')
+                                                  loop_function=loop if infer else None)
         # output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size])
         output = tf.reshape(tf.concat(axis=1, values=outputs), [-1, args.rnn_size])
         self.logits = tf.nn.xw_plus_b(output, softmax_w, softmax_b)
